e349b7b048c7c572af36048e6f844286f726f99c
tumanitochka <olegnastyle@yandex.ru>
2024-01-03 08:13:48 +0300

    update

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function closeLetter(){
 function onLetter(){
     document.getElementById('letter').style.display = 'flex'
 }
+
+
+
+// Факультет
+
+let random = Math.floor(Math.random() * 2)
+
+function onFucult(){
+    if(random = 1){
+        document.getElementById('gryffindor').style.transform = 'scale(1)'
+    } else if(random = 0) {
+        document.getElementById('slytherin').style.transform = 'scale(1)'
+    } else {
+        document.getElementById('slytherin').style.transform = 'scale(0)'
+        document.getElementById('gryffindor').style.transform = 'scale(0)'
+    }
+}
+
+
+console.log(random)
